4157338a265e6d0c0793eca0f3ed1eaf77119dfb
Daniel Tejada <dastte93@gmail.com>
2022-01-05 00:04:16 -0500

    Agregando componentes html

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import './styles.css';
 
 import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
 
 const todoList = new TodoList();
-const tarea = new Todo('Aprender JavaScript');
-const tarea2 = new Todo('Comprar un unicornio');
 
 
- todoList.nuevoTodo( tarea );
- todoList.nuevoTodo( tarea2 );
+const tarea = new Todo('Aprender JavaScript!!');
+todoList.nuevoTodo(tarea);
 
- console.log(todoList);
+tarea.completado = false;
+
+
+console.log(todoList);
+
+crearTodoHtml( tarea );
