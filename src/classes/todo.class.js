277ab71b9202f998ff90d913d01ae6923174636f
Daniel Tejada <dastte93@gmail.com>
2022-01-05 00:24:33 -0500

    Se crearon las primeras clases

---
+++ src/classes/todo.class.js
@@ -0,0 +1,16 @@
+
+export class Todo {
+
+
+    constructor( tarea ) {
+
+        this.tarea = tarea;
+
+        this.id = new Date().getTime();
+        this.completado = false;
+        this.creado = new Date();
+        
+    }
+}
+
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-import { saludar } from './js/componentes';
 import './styles.css';
 
-const nombre = 'Fernando';
+import { Todo, TodoList } from './classes';
 
-saludar( nombre );
+const todoList = new TodoList();
+const tarea = new Todo('Aprender JavaScript');
+const tarea2 = new Todo('Comprar un unicornio');
+
+
+ todoList.nuevoTodo( tarea );
+ todoList.nuevoTodo( tarea2 );
+
+ console.log(todoList);
